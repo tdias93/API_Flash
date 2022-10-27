2bbbdbcf02ceb85394443278937d097d03cabf2c
tdias93 <thiago.olimpio93@gmail.com>
2022-10-27 18:13:21 -0300

    first commit

---
+++ venv/main.py
@@ -0,0 +1,73 @@
+from flask import Flask, make_response, jsonify, request
+from jsonschema import validate, ValidationError
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+@app.route('/teste', methods=['POST'])
+def creat_nf():
+
+    try :
+        validate(instance=request.json, schema=schema)
+
+        nf = request.json
+        dados.append(nf)
+
+        retorno = make_response(jsonify(dados))
+    
+    except ValidationError as err:
+
+        retorno = make_response(jsonify(mensage = f'ERRO DE SINTAXE - {err.message}'))
+
+    except :
+
+        retorno = make_response(jsonify(mensage = f'ERRO DE SINTAXE'))
+
+    return retorno
+
+
+schema = {
+    "type": "object",
+    "properties": {
+        "cnpj": {
+            "type" : "string"
+            },
+        "nome": {
+            "type" : "string"
+            },
+        "nota_fiscal": {
+            "type" : "string"
+            },
+        "conhecimento": {
+            "type" : "string"
+            },
+        "data_emissao": {
+            "type" : "string"
+            },
+        "comprovante": {
+            "type" : "string"
+            },
+    },
+    "required":[
+      "cnpj",
+      "nota_fiscal",
+      "conhecimento",
+      "data_emissao",
+      "comprovante"
+   ]
+}
+
+dados = [
+    {
+        "cnpj": "10750264000116",
+        "nome": "TESTE 01",
+        "nota_fiscal": "1234",
+        "conhecimento": "5678",
+        "data_emissao": "10/05/2022",
+        "comprovante": "IMAGEM - BINARIA"
+    }
+]
+
+# https://www.youtube.com/watch?v=LP8besicfH4
+
+app.run()
